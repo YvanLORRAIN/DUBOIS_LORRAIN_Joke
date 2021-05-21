e71852e708956c395fd7de36070ba65fd8437b4a
YvanLORRAIN <yvan.lorrain@epfedu.fr>
2021-05-21 11:54:48 +0200

    modif

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
-
+st.title("Hello world")
 # Import google analytics
 import os
 import re
 code = """<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-197548657-1"></scrip
+src="https://www.googletagmanager.com/gtag/js?id=UA-197552790-1"></scrip
 t>
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-197548657-1');
+gtag('config', 'UA-197552790-1');
 </script>"""
 a=os.path.dirname(st.__file__) + '/static/index.html'
 with open(a, 'r') as f:
@@ -21,4 +21,4 @@ with open(a, 'r') as f:
 			newdata=re.sub('<head>','<head>' + code, data)
 			f.write(newdata)
 
-st.title("Hello world")
+
