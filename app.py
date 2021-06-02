be97c9ab8d4f65dfb6cf36bce275a712b0c61eb9
YvanLORRAIN <yvan.lorrain@epfedu.fr>
2021-06-02 16:54:33 +0200

    TP3 google trends test

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 import streamlit as st
 import logging
+import requests
+from pytrends.request import TrendReq
+
+
+pytrends = TrendReq(hl='en-US', tz=360)
+pytrends.trending_searches(pn='Obi-Wan Kenobi')
 
 logging.warning("It's a trap!")
 
 st.title("Hello world")
 title = st.text_input('A meme', 'Hello there')
+
+if st.button('Say hello to google'):
+	st.write('Request to google sent')
+	req=requests.get("https://www.google.com/")
+	req2 =requests.get("https://analytics.google.com/analytics/web/?pli=1#/report-home/a197552790w273202703p243378564")
+else:
+	st.write('Request not sent')
+	
+st.markdown(req.cookies._cookies)
+st.text(req2.status_code)
+st.markdown(req2.text)
+
+from html import unescape
+import streamlit.components.v1 as components
+components.html(unescape(req.text))
 
 import os
 import re
